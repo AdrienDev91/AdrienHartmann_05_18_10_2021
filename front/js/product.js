934584fdcaea9a6270ef510427de7a08eeb4aae1
Adrien <adrien.hartmann0@orange.fr>
2021-11-16 14:48:58 +0100

    ajout photo, prix, description page produit (+ essai couleurs)

--- front/js/product.js
+++ front/js/product.js
@@ -1,24 +1,53 @@
 var str = location.href;
 var url = new URL(str);
 var id = url.searchParams.get("id");
-console.log(id);
+//console.log(id);
 
 var data = fetch(`http://localhost:3000/api/products/${id}`)
 
 data
     .then((response) => { return response.json() })
     .then(function(response) {
-        console.log(response);
+        //console.log(response);
+
 
         const kanapName = response.name;
-        console.log(kanapName);
-        const affichage_kanapName = document.querySelector("#title");
-        affichage_kanapName.innerHTML = kanapName
-
+        //console.log(kanapName);
+        const kanapPrice = response.price;
+        //console.log(kanapPrice);
+        const kanapDescription = response.description;
+        //console.log(kanapDescription);
+        const kanapImg = response.imageUrl;
+        //console.log(kanapImg);
+        const kanapImg_altTxt = response.altTxt;
+        //console.log(kanapImg_altTxt);
         const kanapColors = response.colors;
-        console.log(kanapColors);
-        const affichage_kanapColors = document.getElementById("colors").value;
-        console.log(affichage_kanapColors);
+        //console.log(kanapColors);
+
+        //---------------------------------------------------------------------------------//
+
+        const affichage_kanapName = document.querySelector("#title");
+        const affichage_kanapPrice = document.querySelector("#price");
+        const affichage_kanapDescription = document.querySelector("#description");
+        const affichage_kanapImg = document.getElementsByTagName("img").item(5).src = `${kanapImg}`;
+        //console.log(affichage_kanapImg);
+
+        //---------------------------------------------------------------------------------//
+
+        affichage_kanapName.innerHTML = kanapName
+        affichage_kanapPrice.innerHTML = kanapPrice
+        affichage_kanapDescription.innerHTML = kanapDescription
+
+        //---------------------------------------------------------------------------------//
+        var sectionColors = document.querySelector('#colors')
+        kanapColors.forEach((color) => {
+            //  Ajout du code html dans la section
+            sectionColors.innerHTML += `<option value="${kanapColors[0]}">${kanapColors[0]}</option>
+            <option value="${kanapColors[1]}">${kanapColors[1]}</option>
+            <option value="${kanapColors[2]}">${kanapColors[2]}</option>
+            `
+        })
+
 
 
     })
